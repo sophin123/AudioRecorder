eb78588db361263db790ac468c70f7ea4bc69950
paul_sophin <sophin123@gmail.com>
2020-09-15 15:46:37 +1000

    Record Layout Completion

---
+++ app/src/main/java/com/example/microphone/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.microphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onClick(p0: View?) {
+
+    }
+}
---
+++ app/src/main/java/com/example/microphone/RecordFragment.kt
@@ -0,0 +1,17 @@
+package com.example.microphone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RecordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_record, container, false)
+    }
+}
