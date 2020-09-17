21a805cdee7a92ab2c03cdae70ff76d8a2ab7d5d
paul_sophin <sophin123@gmail.com>
2020-09-17 13:48:43 +1000

    Media Player Completion

--- app/src/main/java/com/example/microphone/RecordFragment.kt
+++ app/src/main/java/com/example/microphone/RecordFragment.kt
@@ -1,17 +1,24 @@
 package com.example.microphone
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class RecordFragment : Fragment() {
+class RecordFragment : Fragment(){
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_record, container, false)
+
     }
+
+
+
 }
