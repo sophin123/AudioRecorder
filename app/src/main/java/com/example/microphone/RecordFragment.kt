721491a17f76a8ecd328b7fd1ec7bcc5dd995ae1
paul_sophin <sophin123@gmail.com>
2020-09-17 19:54:56 +1000

    Added Function to mic status

--- app/src/main/java/com/example/microphone/RecordFragment.kt
+++ app/src/main/java/com/example/microphone/RecordFragment.kt
@@ -1,10 +1,15 @@
 package com.example.microphone
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -12,7 +17,11 @@ import androidx.navigation.Navigation
 class RecordFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
-    lateinit var viewList: ImageView
+    lateinit var viewList: ImageButton
+    lateinit var recordBtn: ImageButton
+    lateinit var text: TextView
+
+    private var isRecording: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,20 +32,38 @@ class RecordFragment : Fragment(), View.OnClickListener {
 
     }
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         navController = Navigation.findNavController(view)
         viewList = view.findViewById(R.id.view_record_list)
+        recordBtn = view.findViewById(R.id.record_audio)
+        text = view.findViewById(R.id.record_filename)
 
         viewList.setOnClickListener(this)
+        recordBtn.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?) {
-        when(view?.id){
-            R.id.view_record_list -> navController.navigate(R.id.action_recordFragment_to_audioListFragment)
+    @SuppressLint("UseCompatLoadingForDrawables")
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.record_audio -> {
+                isRecording = if (isRecording) {
+                    recordBtn.setImageDrawable(resources.getDrawable(R.drawable.mic_enabled))
+                    false
+                } else {
+                    recordBtn.setImageDrawable(resources.getDrawable(R.drawable.mic_disabled))
+                    true
+                }
+            }
+
+            R.id.view_record_list -> {
+                navController.navigate(R.id.action_recordFragment_to_audioListFragment)
+                Log.d("Tag", "Record list Clicked")
+            }
         }
     }
-
-
 }
