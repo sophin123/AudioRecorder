10b29b71a5b822bef59c3fcf769ad607675ef700
paul_sophin <sophin123@gmail.com>
2020-09-17 13:54:04 +1000

    Added Function to navigate recordfragment to AudioListFragment

--- app/src/main/java/com/example/microphone/RecordFragment.kt
+++ app/src/main/java/com/example/microphone/RecordFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-class RecordFragment : Fragment(){
+class RecordFragment : Fragment(), View.OnClickListener {
 
-
+    lateinit var navController: NavController
+    lateinit var viewList: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,20 @@ class RecordFragment : Fragment(){
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        viewList = view.findViewById(R.id.view_record_list)
+
+        viewList.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.view_record_list -> navController.navigate(R.id.action_recordFragment_to_audioListFragment)
+        }
+    }
 
 
 }
